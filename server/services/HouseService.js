13031752ee00e7b2893a01d7ee322bfb0bee650a
Josiah Aubin <josiahaubin@gmail.com>
2019-09-04 15:12:36 -0600

    House service changes

--- server/services/HouseService.js
+++ server/services/HouseService.js
@@ -4,10 +4,11 @@ const Schema = mongoose.Schema
 const _model = new Schema({
   bathrooms: { type: Number, required: true },
   bedrooms: { type: Number, required: true },
-  imgUrl: { type: String, default: 'https//placehold.it/200x200' },
+  imgUrl: { type: String, default: 'http://placehold.it/200x200' },
   levels: { type: Number, required: true },
   year: { type: Number, required: true },
-  price: { type: Number, required: true }
+  price: { type: Number, required: true },
+  description: { type: String }
 })
 export default class HouseService {
   get respository() {
