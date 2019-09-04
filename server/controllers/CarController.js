d3b1c5f0345d94dd7ae28eee83da4b35442bae44
Josiah Aubin <josiahaubin@gmail.com>
2019-09-04 14:15:23 -0600

    All CRUD works for Car C&S

--- server/controllers/CarController.js
+++ server/controllers/CarController.js
@@ -1,7 +1,49 @@
 import express from "express"
+import CarService from "../services/CarService";
+
+let _cs = new CarService().respository
 
 export default class CarController {
+
   constructor() {
     this.router = express.Router()
+      .get('', this.getAll)
+      .get('/:id', this.getOne)
+      .post('', this.create)
+      .put('/:id', this.update)
+      .delete('/:id', this.delete)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      let cars = await _cs.find({})
+      res.send(cars)
+    } catch (error) { next(error) }
+  }
+  async getOne(req, res, next) {
+    try {
+      let car = await _cs.findById(req.params.id)
+      res.send(car)
+    } catch (error) { next(error) }
+  }
+  async create(req, res, next) {
+    try {
+      let car = await _cs.create(req.body)
+      res.send(car)
+    } catch (error) { next(error) }
+
+  }
+  async update(req, res, next) {
+    try {
+      let car = await _cs.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+      res.send(car)
+    } catch (error) { next(error) }
+  }
+  async delete(req, res, next) {
+    try {
+      let car = await _cs.findOneAndRemove({ _id: req.params.id })
+      res.send("Deleted")
+
+    } catch (error) { next(error) }
   }
 }
--- server/services/CarService.js
+++ server/services/CarService.js
@@ -1,3 +1,16 @@
-export default class CarService {
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
 
+const _model = new Schema({
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  price: { type: Number, required: true },
+  imgUrl: { type: String, default: "http://placehold.it/200x200" },
+  description: { type: String }
+})
+export default class CarService {
+  get respository() {
+    return mongoose.model('car', _model)
+  }
 }
